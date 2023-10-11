ecc7d9756fbd488b599b33f65a6515c48c2aeb34
wajd alkayyali <wajd.kayyali@gmail.com>
2023-10-11 14:25:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby"];
+
+function findMatching(drivers, name) {
+  const lowercase = name.toLowerCase();
+  const matching = drivers.filter((name) => name.toLowerCase() === lowercase);
+  console.log(matching);
+  return matching;
+}
+
+findMatching(drivers, Bobby);
+
+drivers.length = 0;
+drivers.push("Bobby", "Sammy", "Sally", "Annette", "Sarah", "bobby");
+
+function fuzzyMatch(drivers, name) {
+  const letters = name.slice(0, 2);
+  const match = drivers.filter((name) => name.slice(0, 2) === letters);
+
+  return match;
+}
+drivers.length = 0;
+drivers.push(
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  }
+);
+function matchName(drivers, name) {
+  const match = drivers.filter((drivers) => drivers.name === name);
+  return match;
+}
